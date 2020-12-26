c944cff05802b5d087ff82acd6691e7f60c63dfe
Milos Budalic <milos.budalic@persida-bio.com>
2020-12-26 23:48:06 +0100

    Handling user input...

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { EmailValidator, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -10,6 +11,7 @@ import { LoadingController } from '@ionic/angular';
 })
 export class AuthPage implements OnInit {
 isLoading = false;
+isLogin = true;
 
   constructor(private authService: AuthService,
               private router: Router,
@@ -31,5 +33,29 @@ isLoading = false;
         this.router.navigateByUrl('/places/tabs/discover');
       }, 1500);
     })
+  }
+
+  onSubmit(form: NgForm){
+    if (!form.valid)
+    {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if(this.isLogin)
+    {
+      console.log(email, password);
+    }
+    else 
+    {
+
+    }
+  }
+
+  onSwitchAuthMode() 
+  {
+    this.isLogin = !this.isLogin;
   }
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -11,21 +11,27 @@ export class PlacesService {
       'Manhattan Mansion', 
       'In the heart of New York City.', 
       'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/1200px-Above_Gotham.jpg', 
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p2', 
       "L'Amour Toujours", 
       'A romantic place in Paris!', 
       'https://images.adsttc.com/media/images/5d44/14fa/284d/d1fd/3a00/003d/large_jpg/eiffel-tower-in-paris-151-medium.jpg?1564742900', 
-      189.99
+      189.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p3', 
       'The Foggy Palace', 
       'Not your average city trip!', 
       'https://i.pinimg.com/originals/9c/88/44/9c8844b217bdb6c17db14f51ad2e51a5.jpg', 
-      189.99
+      189.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     )
   ];
 
